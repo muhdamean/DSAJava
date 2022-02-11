176283f1edd794f0508398e1a16158f8e7db451c
muhdamean <muhdamean@gmail.com>
2022-02-11 12:19:15 +0100

    UPDATE: end of doubly linked list

--- list/DoublyLinkedList.java
+++ list/DoublyLinkedList.java
@@ -1,5 +1,7 @@
 package list;
 
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedList {
 
     private ListNode head;
@@ -43,6 +45,20 @@ public class DoublyLinkedList {
         tail=newNode;
         length++;
     }
+
+    //insert first
+    public void insertFirst(int value){
+        ListNode newNode=new ListNode(value);
+        if(isEmpty()){
+            tail=newNode;
+        }else{
+            head.previous=newNode;
+        }
+        newNode.next=head;
+        head=newNode;
+        length++;
+    }
+
     //print elements in DLL
     public void displayForward(){
         if(head==null){
@@ -55,6 +71,7 @@ public class DoublyLinkedList {
         }
         System.out.println("null");
     }
+
     //print elements in DLL backward direction
     public void dispalyBackward(){
         if(tail==null){
@@ -68,16 +85,53 @@ public class DoublyLinkedList {
         System.out.println("null");
     }
 
+    //delete first
+    public ListNode deleteFirst(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        ListNode temp=head;
+        if(head==tail){
+            tail=null;
+        }else{
+            head.next.previous=null;
+        }
+        head=head.next;
+        temp.next=null;
+        return temp;
+    }
+
+    //delete last
+    public ListNode deleteLast(){
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
+        ListNode temp=tail;
+        if(head==tail){
+            tail=null;
+        }else{
+            tail.previous.next=null;
+        }
+        tail=tail.previous;
+        temp.previous=null;
+        return temp;
+    }
+
     public static void main(String[] args) {
         DoublyLinkedList dll=new DoublyLinkedList();
 
-        dll.insertLast(2);
-        dll.insertLast(4);
-        dll.insertLast(5);
-        dll.insertLast(8);
+        dll.insertFirst(2);
+        dll.insertFirst(4);
+        dll.insertFirst(5);
+        dll.insertFirst(8);
 
         dll.displayForward();
         dll.dispalyBackward();
-        
+
+        dll.deleteFirst();
+        dll.displayForward();
+
+        dll.deleteLast();
+        dll.displayForward();
     }
 }
