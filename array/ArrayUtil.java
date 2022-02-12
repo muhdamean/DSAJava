17a135e5e85c9fd81e5b2965bb01064695f5db4b
muhdamean <muhdamean@gmail.com>
2022-02-12 13:02:54 +0100

    CREATE: array class

---
+++ array/ArrayUtil.java
@@ -0,0 +1,17 @@
+package array;
+
+public class ArrayUtil {
+    
+    public void printArray(int[] arr){
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ArrayUtil arrUtil=new ArrayUtil();
+        arrUtil.printArray(new int[]{4,2,6,1,3});
+    }
+}
