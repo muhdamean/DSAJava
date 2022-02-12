f84cb7f9d78d3a006082a72359a65fa3af60a222
muhdamean <muhdamean@gmail.com>
2022-02-12 12:30:36 +0100

    UPDATE: end of Circular Singly Linked List

--- list/CircularSinglyLinkedList.java
+++ list/CircularSinglyLinkedList.java
@@ -1,5 +1,7 @@
 package list;
 
+import java.util.NoSuchElementException;
+
 public class CircularSinglyLinkedList {
     private ListNode last;
     private int length;
@@ -52,11 +54,73 @@ public class CircularSinglyLinkedList {
             first=first.next;
         }
         System.out.print(first.data+" ");
+        System.out.println();
+    }
+
+    //insert first
+    public void insertFirst(int data){
+        ListNode temp=new ListNode(data);
+        if(last==null){
+            last=temp;
+        }
+        else{
+            temp.next=last.next;
+        }
+        last.next=temp;
+        length++;
+    }
+
+    //insert last
+    public void insertLast(int data){
+        ListNode temp=new ListNode(data);
+        if(last==null){
+            last=temp;
+            last.next=last;
+        }else{
+            temp.next=last.next;
+            last.next=temp;
+            last=temp;
+        }
+        length++;
+    }
+
+    //remove first node
+    public int removeFirst(){
+        if(isEmpty()){
+            throw new NoSuchElementException("Circular Singly Linked List is already empty");
+        }
+        ListNode temp=last.next;
+        int result=temp.data;
+        if(last.next==last){
+            last=null;
+        }
+        else{
+            last.next=temp.next;
+        }
+        //temp.next=null;
+        length--;
+        return result;
     }
     public static void main(String[] args) {
         CircularSinglyLinkedList csll=new CircularSinglyLinkedList();
 
-        csll.createCircularLinkedList();
+        //csll.createCircularLinkedList();
+
+        csll.insertLast(1);
+        csll.insertLast(5);
+        csll.insertLast(8);
+        csll.insertLast(15);
+
+        csll.display();
+
+        csll.insertFirst(7);
+        csll.display();
+
+        csll.insertLast(9);
+        csll.display();
+
+         
+        System.out.println(csll.removeFirst());
         csll.display();
     }
 
