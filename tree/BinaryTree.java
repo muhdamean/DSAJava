fffcb72c2eab14f45f618349e3cad9c26046fcfd
muhdamean <muhdamean@gmail.com>
2022-03-29 08:26:32 +0100

    CODE: add iterativePreOrder in binaryTree

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -47,9 +47,9 @@ public class BinaryTree {
         if(root==null){//base case
             return;
         }
-        System.out.print(root.data+ " ");
-        postOrder(root.right);
         postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+ " "); 
     }
 
     //recursive inOrder traversal
@@ -82,20 +82,30 @@ public class BinaryTree {
     }
     //iterative postOrder traversal
     public void iterativePostOrder(TreeNode root){
-        if(root==null){
-            return;
-        }
+        TreeNode current=root;
         Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode temp=stack.pop();
-            System.out.print(temp.data+" ");
-            if(temp.left!=null){
-                stack.push(temp.left);
+        while(current!=null || !stack.isEmpty()){
+            if(current!=null){
+                stack.push(current);
+                current=current.left; //traverse the left node
+            }else{
+                //traverse the right node if left is null
+                TreeNode temp=stack.peek().right; 
+
+                if(temp==null){//check right node is null
+                    temp=stack.pop(); //pop the parent node from stack
+                    System.out.print(temp.data+ " "); //print the parent node
+
+                    //check if the temp is the right node of the current prent in stack
+                    while(!stack.isEmpty() && temp==stack.peek().right){ 
+                        temp=stack.pop();
+                        System.out.print(temp.data+ " ");
+                    }
+                }else{
+                    current=temp;
+                }
             }
-            if(temp.right!=null){
-                stack.push(temp.right);
-            }
+            
         }
     }
     //iterative inOrder traversal
@@ -119,13 +129,23 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         BinaryTree binaryTree=new BinaryTree();
-        binaryTree.createBinaryTree();
-        System.out.println("Recursive Pre-Order");
+        binaryTree.createBinaryTree(); //create binary tree
+
+        System.out.println("Recursive PreOrder");
         binaryTree.preOrder(binaryTree.root);
-        System.out.println("\nRecursive Post-Order");
+        System.out.println("\nRecursive PostOrder");
         binaryTree.postOrder(binaryTree.root);
+        System.out.println("\nRecursive InOrder");
+        binaryTree.inOrder(binaryTree.root);
+        System.out.println();
         //iterative preOrder
-        System.out.println("\nIterative Pre-Order");
+        System.out.println("\nIterative PreOrder");
         binaryTree.iterativePreOrder(binaryTree.root);
+        //iterative postOrder
+        System.out.println("\nIterative PostOrder");
+        binaryTree.iterativePostOrder(binaryTree.root);
+        //iterative postOrder
+        System.out.println("\nIterative InOrder");
+        binaryTree.iterativeInOrder(binaryTree.root);
     }
 }
